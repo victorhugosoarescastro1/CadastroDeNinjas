9acea9391302028142da5e022ed53ea5565f57e3
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-04-09 16:29:57 -0300

    Criação da classe para entidade

---
+++ src/main/java/dev/Java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,61 @@
+package dev.Java10x.CadastroDeNinjas;
+
+//JPA = JAVA PERSISTENCE API
+import jakarta.persistence.*;
+
+@Entity // Ele transforma uma classe em uma entidade no Banco de Dados (BD)
+@Table (name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String clube;
+    String tecnicas;
+    int rank;
+
+    public NinjaModel(){
+    }
+
+    public NinjaModel(String nome, String clube, String tecnicas, int rank){
+        this.nome = nome;
+        this.clube = clube;
+        this.tecnicas = tecnicas;
+        this.rank = rank;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getClube() {
+        return clube;
+    }
+
+    public void setClube(String clube) {
+        this.clube = clube;
+    }
+
+    public String getTecnicas() {
+        return tecnicas;
+    }
+
+    public void setTecnicas(String tecnicas) {
+        this.tecnicas = tecnicas;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+}
